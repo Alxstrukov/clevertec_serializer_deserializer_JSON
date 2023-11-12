0be03f5dd036b3ae20779853b75b72526bb4a014
Alexandr Strukov <alxstrukov@mail.ru>
2023-11-12 22:32:44 +0300

    modified class Serializer

--- src/main/java/by/clevertec/serialization/interfaces/ToJsonConvertible.java
+++ src/main/java/by/clevertec/serialization/interfaces/ToJsonConvertible.java
@@ -8,18 +8,18 @@ public interface ToJsonConvertible {
             throws IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
     void convertIntegratedClassObjectToJson(String fieldName, StringBuilder stringJson, Field field,
-                                            Object sourceObject) throws IllegalAccessException;
+                                            Object sourceObject) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
     void convertNullToJson(String fieldName, StringBuilder stringJson, Field field, Object sourceObject)
-            throws IllegalAccessException;
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
     void convertWrapperOrPrimitiveToJson(Class<?> fieldType, String fieldName, StringBuilder stringJson, Field field,
                                          Object sourceObject) throws IllegalAccessException;
 
-    void convertCollectionToJson(Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
-            throws IllegalAccessException;
+    void convertCollectionToJson(Class<?> fieldType,Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
-    void convertMapToJson(Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
+    void convertMapToJson(Class<?> fieldType,Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
             throws IllegalAccessException, InvocationTargetException, NoSuchMethodException;
 
     void convertArrayToJson(Class<?> fieldType, Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
--- src/main/java/by/clevertec/serialization/model/Serializer.java
+++ src/main/java/by/clevertec/serialization/model/Serializer.java
@@ -30,36 +30,23 @@ public class Serializer extends AbstractMapper implements ToJsonConvertible {
 
             fields[i].setAccessible(true);
 
-            if (isWrapperOrPrimitive(fieldType)) {
-                convertWrapperOrPrimitiveToJson(fieldType, fieldName, stringJson, fields[i], sourceObject);
-            } else {
-                if (isCollection(fieldType)) {
-                    convertCollectionToJson(fields[i], sourceObject, stringJson, fieldName);
-                } else {
-                    if (isMap(fieldType)) {
-                        convertMapToJson(fields[i], sourceObject, stringJson, fieldName);
-                    } else {
-                        if (isArray(fieldType)) {
-                            convertArrayToJson(fieldType, fields[i], sourceObject, stringJson, fieldName);
-                        } else {
-                            if (fields[i].get(sourceObject) == null) {
-                                convertNullToJson(fieldName, stringJson, fields[i], sourceObject);
-                            } else {
-                                if (isIntegratedClassObject(fields[i].get(sourceObject))) {
-                                    convertIntegratedClassObjectToJson(fieldName, stringJson, fields[i], sourceObject);
-                                } else {
-                                    convertCustomClassObjectToJson(fieldName, stringJson, fields[i], sourceObject);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            mapFieldToJson(fieldType, fieldName, stringJson, fields[i], sourceObject);
+
             if (i == (fields.length - 1)) stringJson.deleteCharAt(stringJson.length() - 1);
         }
         stringJson.append("}");
         return stringJson.toString();
     }
+
+    private void mapFieldToJson(Class<?> fieldType, String fieldName, StringBuilder stringJson, Field field,
+                                Object sourceObject) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        if (isWrapperOrPrimitive(fieldType)) {
+            convertWrapperOrPrimitiveToJson(fieldType, fieldName, stringJson, field, sourceObject);
+        } else {
+            convertCollectionToJson(fieldType,field, sourceObject, stringJson, fieldName);
+        }
+    }
+
 
     public void convertCustomClassObjectToJson(String fieldName, StringBuilder stringJson, Field field, Object sourceObject)
             throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
@@ -71,34 +58,43 @@ public class Serializer extends AbstractMapper implements ToJsonConvertible {
     }
 
     public void convertIntegratedClassObjectToJson(String fieldName, StringBuilder stringJson, Field field,
-                                                    Object sourceObject) throws IllegalAccessException {
-        if (isLocalDateClassObject(field.get(sourceObject))) {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy,MM,dd");
-            String dateOfFormat = ((LocalDate) field.get(sourceObject)).format(formatter);
-            String[] dateElements = dateOfFormat.split(",");
-            stringJson.append("\"")
-                    .append(fieldName)
-                    .append("\":")
-                    .append(Arrays.toString(dateElements).replaceAll(" ", ""))
-                    .append(",");
+                                                   Object sourceObject)
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        if (isIntegratedClassObject(field.get(sourceObject))) {
+            if (isLocalDateClassObject(field.get(sourceObject))) {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy,MM,dd");
+                String dateOfFormat = ((LocalDate) field.get(sourceObject)).format(formatter);
+                String[] dateElements = dateOfFormat.split(",");
+                stringJson.append("\"")
+                        .append(fieldName)
+                        .append("\":")
+                        .append(Arrays.toString(dateElements).replaceAll(" ", ""))
+                        .append(",");
+            } else {
+                stringJson.append("\"")
+                        .append(fieldName)
+                        .append("\":")
+                        .append("\"")
+                        .append(field.get(sourceObject))
+                        .append("\"")
+                        .append(",");
+            }
         } else {
-            stringJson.append("\"")
-                    .append(fieldName)
-                    .append("\":")
-                    .append("\"")
-                    .append(field.get(sourceObject))
-                    .append("\"")
-                    .append(",");
+            convertCustomClassObjectToJson(fieldName, stringJson, field, sourceObject);
         }
     }
 
     public void convertNullToJson(String fieldName, StringBuilder stringJson, Field field, Object sourceObject)
-            throws IllegalAccessException {
-        stringJson.append("\"")
-                .append(fieldName)
-                .append("\":")
-                .append(field.get(sourceObject))
-                .append(",");
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        if (field.get(sourceObject) == null) {
+            stringJson.append("\"")
+                    .append(fieldName)
+                    .append("\":")
+                    .append(field.get(sourceObject))
+                    .append(",");
+        } else {
+            convertIntegratedClassObjectToJson(fieldName, stringJson, field, sourceObject);
+        }
     }
 
     public void convertWrapperOrPrimitiveToJson(Class<?> fieldType, String fieldName, StringBuilder stringJson,
@@ -118,59 +114,79 @@ public class Serializer extends AbstractMapper implements ToJsonConvertible {
         }
     }
 
-    public void convertCollectionToJson(Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
-            throws IllegalAccessException {
-        Collection list = (Collection) field.get(sourceObject);
-        stringJson.append("\"")
-                .append(fieldName)
-                .append("\":[");
-        if (isCollectionComponentWrapperOrPrimitive(list)) {
-            list.forEach(e -> stringJson.append(e).append(','));
-            stringJson.deleteCharAt(stringJson.length() - 1).append("],");
-        } else {
-            list.forEach(e -> {
-                try {
-                    stringJson.append(mapObjectToJSON(e)).append(',');
-                } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException ex) {
-                    ex.printStackTrace();
-                }
-            });
-            stringJson.deleteCharAt(stringJson.length() - 1).append("],");
-        }
-    }
-
-    public void convertMapToJson(Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
+    public void convertCollectionToJson(Class<?> fieldType, Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
             throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
 
-        Map map = (Map) field.get(sourceObject);
-        stringJson.append("\"")
-                .append(fieldName)
-                .append("\":{");
-
-        Object[] keyObjects = map.keySet().toArray();
-        for (Object key : keyObjects) {
-            if (isIntegratedClassObject(map.get(key))) {
-                stringJson.append('\"').append(key).append('\"')
-                        .append(":\"")
-                        .append(mapObjectToJSON(map.get(key)))
-                        .append("\",");
+        if (isCollection(fieldType)) {
+            Collection list = (Collection) field.get(sourceObject);
+            stringJson.append("\"")
+                    .append(fieldName)
+                    .append("\":[");
+            if (isCollectionComponentWrapperOrPrimitive(list)) {
+                list.forEach(e -> stringJson.append(e).append(','));
+                stringJson.deleteCharAt(stringJson.length() - 1).append("],");
             } else {
-                stringJson.append('\"').append(key).append('\"')
-                        .append(":")
-                        .append(mapObjectToJSON(map.get(key)))
-                        .append(",");
+                list.forEach(e -> {
+                    try {
+                        stringJson.append(mapObjectToJSON(e)).append(',');
+                    } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException ex) {
+                        ex.printStackTrace();
+                    }
+                });
+                stringJson.deleteCharAt(stringJson.length() - 1).append("],");
             }
+        } else {
+            convertMapToJson(fieldType,field, sourceObject, stringJson, fieldName);
         }
-        stringJson.deleteCharAt(stringJson.length() - 1).append("},");
+
+    }
+
+    public void convertMapToJson(Class<?> fieldType, Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+
+        if (isMap(fieldType)) {
+            Map map = (Map) field.get(sourceObject);
+            stringJson.append("\"")
+                    .append(fieldName)
+                    .append("\":{");
+
+            Object[] keyObjects = map.keySet().toArray();
+            for (Object key : keyObjects) {
+                if (isIntegratedClassObject(map.get(key))) {
+                    stringJson.append('\"').append(key).append('\"')
+                            .append(":\"")
+                            .append(mapObjectToJSON(map.get(key)))
+                            .append("\",");
+                } else {
+                    stringJson.append('\"').append(key).append('\"')
+                            .append(":")
+                            .append(mapObjectToJSON(map.get(key)))
+                            .append(",");
+                }
+            }
+            stringJson.deleteCharAt(stringJson.length() - 1).append("},");
+        } else {
+            convertArrayToJson(fieldType, field, sourceObject, stringJson, fieldName);
+        }
     }
 
     public void convertArrayToJson(Class<?> fieldType, Field field, Object sourceObject, StringBuilder stringJson, String fieldName)
             throws IllegalAccessException {
-        String primitiveArray = castPrimitiveArrayToString(fieldType, field.get(sourceObject));
-        stringJson.append("\"")
-                .append(fieldName)
-                .append("\":")
-                .append(primitiveArray)
-                .append(',');
+
+        if (isArray(fieldType)) {
+            String primitiveArray = castPrimitiveArrayToString(fieldType, field.get(sourceObject));
+            stringJson.append("\"")
+                    .append(fieldName)
+                    .append("\":")
+                    .append(primitiveArray)
+                    .append(',');
+        } else {
+            try {
+                convertNullToJson(fieldName, stringJson, field, sourceObject);
+            } catch (InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
